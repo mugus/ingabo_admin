dada2baf6720156f4aee28af46410eb5e21a7402
mugus <muhozagustave1213@gmail.com>
2022-07-12 17:17:48 +0200

    add data for modal for product detailss

--- src/screens/includes/ListProducts.js
+++ src/screens/includes/ListProducts.js
@@ -35,7 +35,21 @@ const style = {
 
 export default function ListProducts() {
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
+    // const handleOpen = () => setOpen(true);
+    const [pro_modal, setPro_modal] = useState([]);
+    const handleOpen = (product_id) =>{
+      axios.get(`http://localhost:4000/api/v1/products/${product_id}`).then(res => {
+        // axios.get('http://197.243.14.102:4000/api/v1/products').then(res => {
+          // setProduct(res.data.product);
+          setPro_modal(res.data.product)
+          // console.log("Product: ", res.data.product);
+          setOpen(true);
+      }).catch(err=>{
+          console.log(err);
+      })
+    }
+    let photo_modal = 'http://localhost:4000/uploads/'+pro_modal.image;
+    
     const handleClose = () => setOpen(false);
     
     const [product, setProduct] = useState([]);
@@ -57,7 +71,7 @@ export default function ListProducts() {
         product ? setisready(true) : setisready(false)
       }, [])
 
-      console.log("Data: ",product);
+      // console.log("Data: ",product);
     return (
         <>
         {
@@ -68,7 +82,7 @@ export default function ListProducts() {
                     let photo = 'http://localhost:4000/uploads/'+product.image;
                     // let photo = 'http://197.243.14.102:4000/uploads/'+product.image;
                     return (
-                        <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+                        <Grid item xs={12} sm={6} md={4} lg={4} xl={3} key={product.product_id}>
 
                             <Card sx={{ width: '100%' }}>
                                 <CardMedia
@@ -88,7 +102,7 @@ export default function ListProducts() {
                                 <CardActions>
                                     
                                 <Button variant="outlined" size="small" onClick={()=> alert("Usage is ready")}>Product usage</Button>
-                                <Button variant="outlined" size="small" onClick={handleOpen}>Edit Details</Button>
+                                <Button variant="outlined" size="small" value={product.product_id} onClick={e => handleOpen(e.target.value)}>Edit Details</Button>
                                 </CardActions>
                             </Card>
 
@@ -110,30 +124,38 @@ export default function ListProducts() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h5" component="h2">
-            Text in a modal
-          </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            
+            <Grid container >
+              <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+                <img src={photo_modal} width="150" height='200' />
+              </Grid>
+              <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+                <h4>Update <b>{pro_modal.name}</b> Product Deatils</h4>
+                <br/>
+                <Typography color='primary'>Change Image</Typography>
+                <input type="file" name="image" label="file"  />
+              </Grid>
+            </Grid>
           <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '100%' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="standard-basic" label="Product Name" variant="standard"/>
-        <TextField id="standard-basic" label="Product Category" variant="standard" />
-        <TextField id="standard-basic" label="Product Size" variant="standard" />
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '100%' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+        <TextField id="standard-basic" value={pro_modal.name} label="Product Name" variant="standard"/>
+        <TextField id="standard-basic" value={pro_modal.category} label="Product Category" variant="standard" />
+        <TextField id="standard-basic" value={pro_modal.size} label="Product Size" variant="standard" />
         <TextField
+            value={pro_modal.description}
             id="standard-multiline-static"
             label="Product details(Description)"
             multiline
             rows={3}
             variant="standard"
           />          
-        <input type="file" name="image" label="file"  />
+        
        
         <Button className="CreatePro" variant="contained" color="success">
           Update
