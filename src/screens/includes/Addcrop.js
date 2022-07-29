9bed29ad71d7ab0148d72815e8f9f9c23125d8be
mugus <muhozagustave1213@gmail.com>
2022-07-29 11:05:10 +0200

    add crop kinya loading button

--- src/screens/includes/Addcrop.js
+++ src/screens/includes/Addcrop.js
@@ -3,9 +3,11 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
 import '../../styles/Products.css';
 import axios from 'axios';
 import Alert from '@mui/material/Alert';
+import $ from 'jquery'; 
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -26,8 +28,20 @@ export default function Addcrop() {
   const [alertclass, setAlertclass] = useState("");
   const token = localStorage.getItem('token');
 
+  $(document).ready(function(){
+    $('.CreatePro').show()
+    $('.Loading_btn').hide()
+  })
+
+
   const handleAddCrop= async (e)=>{
     e.preventDefault();
+    $(document).ready(function(){
+      $('.CreatePro').hide()
+      $('.Loading_btn').show()
+    })
+
+    
     const data = new FormData(); 
     data.append("name", name);
     data.append("image", image);
@@ -77,19 +91,56 @@ export default function Addcrop() {
         noValidate
         autoComplete="off"
       >
-        <div style={{ paddingLeft: 40 }}>
-          {msg ? <Alert severity={alertclass}>{msg}</Alert> : <></> }
-        </div>
+        {
+        language == 1 ?
+        <>
+        <TextField id="standard-basic" label="Izina ry'igihingwa" variant="standard" value={name} onChange={(e) => setName(e.target.value)} />
 
+        <input type="file" name="image" label="file" onChange={e => {
+                            const image = e.target.files[0];
+                            setImage(image)
+                          }} required/>
+        <Button variant="contained" color="success" className='CreatePro' onClick={handleAddCrop}>
+          Andika Igihingwa
+        </Button>
+        <LoadingButton
+            loading
+            loadingPosition="center"
+            variant="contained"
+            color="primary"
+            className='Loading_btn'
+          >
+            Tegereza gato
+        </LoadingButton>
+        </>
+        :
+        <>
         <TextField id="standard-basic" label="Crop Name" variant="standard" value={name} onChange={(e) => setName(e.target.value)} />
 
         <input type="file" name="image" label="file" onChange={e => {
                             const image = e.target.files[0];
                             setImage(image)
                           }} required/>
-        <Button variant="contained" color="success" onClick={handleAddCrop}>
+        <Button variant="contained" color="success" className='CreatePro' onClick={handleAddCrop}>
           Create Crop
         </Button>
+          <LoadingButton
+              loading
+              loadingPosition="center"
+              variant="contained"
+              color="primary"
+              className='Loading_btn'
+            >
+              Wait
+          </LoadingButton>
+        </>
+
+        }
+        <div style={{ paddingLeft: 40 }}>
+          {msg ? <Alert severity={alertclass}>{msg}</Alert> : <></> }
+        </div>
+
+        
       </Box>
 
     {/* </Grid> */}
